122701d79fa57604d3a92b8976b1cc8cb5f4e42c
Zooff <dimitri.escalle@orange.fr>
2017-05-23 11:47:47 +0200

    add left menu

--- client/js/controller/main.controller.js
+++ client/js/controller/main.controller.js
@@ -3,4 +3,9 @@ angular.module('dashboardInfra.controller')
 .controller('mainCtrl', function(){
   var main = this;
   main.black = true;
+  main.style = false;
+
+  main.changeStyle = function(){
+    main.style = !main.style;
+  }
 })
