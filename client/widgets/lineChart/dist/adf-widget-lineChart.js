9c6203cbe94a04290cd51d867604fb9f3174f6c2
Zooff <dimitri.escalle@orange.fr>
2017-07-05 17:05:42 +0200

    corection bug master/slave

--- client/widgets/lineChart/dist/adf-widget-lineChart.js
+++ client/widgets/lineChart/dist/adf-widget-lineChart.js
@@ -27,8 +27,8 @@ function lineChartWidget(dashboardProvider){
 }
 lineChartWidget.$inject = ["dashboardProvider"];
 
-angular.module("adf.widget.lineChart").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/lineChart/src/edit/edit.html","<script type=text/ng-template id=autocomplete.html><a> <span ng-bind-html=\"match.model.url | uibTypeaheadHighlight:query\"></span> | <small ng-bind-html=\"match.model.desc | uibTypeaheadHighlight:query\"></small> </a></script><form role=form><div class=form-group><label class=sr-only for=desc>Description</label> <input type=text id=desc class=form-control ng-model=config.desc placeholder=\"Enter Description of the data\"></div><hr><input type=radio ng-model=config.mode value=easy id=easy> <label for=easy>Mode Facile</label> <input type=radio ng-model=config.mode value=std id=std> <label for=std>Mode Standard</label> <input type=radio ng-model=config.mode value=exp id=exp> <label for=exp>Mode Expert</label><div class=form-group ng-if=\"config.mode == \'easy\'\"><easy-mode config=config><easy-mode></easy-mode></easy-mode></div><div class=form-group ng-if=\"config.mode == \'std\'\"><div class=form-group><label for=sample>Datasources</label> <input id=sample type=text class=form-control ng-model=config.databaseStandard placeholder=\"Type du Check\" autocomplete=off uib-typeahead=\"database for database in graph.getDatabase($viewValue)\" typeahead-min-length=0 typeahead-on-select=graph.getColumns(config.databaseStandard)></div><div class=form-group><label for=sample>Label</label> <input id=sample type=text class=form-control ng-model=config.columnStandard autocomplete=off uib-typeahead=\"col as col.name for col in graph.column\" typeahead-min-length=0></div><div class=form-group><label for=standardTest>Condition :</label></div><p ng-hide=config.principalCol.length>Choissisez une datasource !</p><div ng-if=config.principalCol.length><label><small>Choix de la Référence</small></label><query-builder group=config.condition.group fields=config.principalCol database=config.databaseStandard></query-builder></div><div ng-if=\"config.condition.group.rules[0] && config.condition.group.rules[0].data\"><label><small>SEC</small></label><query-builder group=config.condition2.group fields=config.otherCol database=config.databaseStandard></query-builder></div></div><div ng-if=\"config.mode == \'exp\'\"><expert-mode config=config></expert-mode></div><hr><input type=checkbox ng-model=config.listener> <label>Slave</label><div ng-if=config.listener><div class=form-group><label>Master Column</label> <input ng-model=config.slaveValue type=text></div></div><div><label>Graph</label></div><div><label>Label</label></div><div class=form-group><label class=sr-only for=label>Label</label> <input type=text id=label class=form-control ng-model=config.label placeholder=Label uib-typeahead=\"key for key in config.key\" typeahead-min-length=0 autocomplete=off></div><div><label>Value</label></div><div class=form-group><label class=sr-only for=value>Value</label> <input type=text id=value class=form-control ng-model=config.value placeholder=\"Première Donnée\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0></div><div class=form-group><label class=sr-only for=value2>Value2</label> <input type=text id=value2 class=form-control ng-model=config.value2 placeholder=\"Seconde Donnée\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0></div><div ng-repeat=\"line in config.lines\"><div class=\"form-group form-inline padding-bottom\"><label class=sr-only for=value2>Value {{$index + 2}}</label> <input type=text id=value2 class=form-control ng-model=line.value placeholder=\"Donnée {{$index + 3}}\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0> <button type=button class=\"btn btn-warning\" ng-click=graph.removeLine($index)><i class=\"fa fa-minus\"></i> Remove</button></div></div><button type=button class=\"btn btn-primary\" ng-click=graph.addLine()><i class=\"fa fa-plus\"></i> Ajouter une ligne</button><div><label>Type du graph</label></div><div class=form-group><label class=sr-only for=sample>Chart Type</label><select class=form-control ng-model=config.type><option value=line>Line</option><option value=bar>Bar</option><option value=horizontalBar>Horizontal Bar</option><option value=radar>Radar</option></select></div><div><label ng-click=\"isCollapsed = !isCollapsed\">Chart Option <span ng-hide=isCollapsed class=\"glyphicon glyphicon-triangle-bottom\" aria-hidden=true></span> <span ng-show=isCollapsed class=\"glyphicon glyphicon-triangle-top\" aria-hidden=true></span></label></div><div ng-show=isCollapsed><input type=checkbox ng-model=config.noPoint><label>No point</label><div ng-repeat=\"line in config.series\" ng-if=\"$index % 2 == 0\" class=row><div class=col-md-6><label for=color>{{line}} :</label><color-picker ng-model=config.color[$index] options=\"{format : \'hexString\'}\"></color-picker></div><div class=col-md-6 ng-if=\"config.series.length > $index +1\"><label for=color>{{config.series[$index +1]}} :</label><color-picker ng-model=config.color[$index+1] options=\"{format : \'hexString\'}\"></color-picker></div></div><div class=row><div class=col-md-6><label for=min>Valeur Minimum</label> <input type=text id=min ng-model=config.minValue></div><div class=col-md-6><label for=max>Valeur Maximum</label> <input type=text id=max ng-model=config.maxValue></div></div><label for=step>Step</label> <input type=text id=step ng-model=config.step></div></form>");
-$templateCache.put("{widgetsPath}/lineChart/src/view/view.html","<div><div ng-hide=graph.label class=\"alert alert-info\" role=alert>Please insert a url to the widget configuration</div><div ng-show=graph.label><div><canvas id=graph class=chart-base chart-type=graph.type chart-data=graph.value chart-labels=graph.label chart-series=graph.series chart-options=graph.options chart-colors=graph.color></canvas></div><div><p class=text-center>{{graph.desc}}</p></div><my-export chart=graph.chart></my-export></div></div>");}]);
+angular.module("adf.widget.lineChart").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/lineChart/src/edit/edit.html","<script type=text/ng-template id=autocomplete.html><a> <span ng-bind-html=\"match.model.url | uibTypeaheadHighlight:query\"></span> | <small ng-bind-html=\"match.model.desc | uibTypeaheadHighlight:query\"></small> </a></script><form role=form><div class=form-group><label class=sr-only for=desc>Description</label> <input type=text id=desc class=form-control ng-model=config.desc placeholder=\"Enter Description of the data\"></div><hr><input type=radio ng-model=config.mode value=easy id=easy> <label for=easy>Mode Facile</label> <input type=radio ng-model=config.mode value=std id=std> <label for=std>Mode Standard</label> <input type=radio ng-model=config.mode value=exp id=exp> <label for=exp>Mode Expert</label><div class=form-group ng-if=\"config.mode == \'easy\'\"><easy-mode config=config><easy-mode></easy-mode></easy-mode></div><div class=form-group ng-if=\"config.mode == \'std\'\"><div class=form-group><label for=sample>Datasources</label> <input id=sample type=text class=form-control ng-model=config.databaseStandard placeholder=\"Type du Check\" autocomplete=off uib-typeahead=\"database for database in graph.getDatabase($viewValue)\" typeahead-min-length=0 typeahead-on-select=graph.getColumns(config.databaseStandard)></div><div class=form-group><label for=sample>Label</label> <input id=sample type=text class=form-control ng-model=config.columnStandard autocomplete=off uib-typeahead=\"col as col.name for col in graph.column\" typeahead-min-length=0></div><div class=form-group><label for=standardTest>Condition :</label></div><p ng-hide=config.principalCol.length>Choissisez une datasource !</p><div ng-if=config.principalCol.length><label><small>Choix de la Référence</small></label><query-builder group=config.condition.group fields=config.principalCol database=config.databaseStandard></query-builder></div><div ng-if=\"config.condition.group.rules[0] && config.condition.group.rules[0].data\"><label><small>SEC</small></label><query-builder group=config.condition2.group fields=config.otherCol database=config.databaseStandard></query-builder></div></div><div ng-if=\"config.mode == \'exp\'\"><expert-mode config=config></expert-mode></div><hr><input type=checkbox ng-model=config.listener> <label>Slave</label><div ng-if=config.listener><div class=form-group><label>Master Column</label> <input ng-model=config.slaveValue type=text></div></div><div><label>Graph</label></div><div><label>Label</label></div><div class=form-group><label class=sr-only for=label>Label</label> <input type=text id=label class=form-control ng-model=config.label placeholder=Label uib-typeahead=\"key for key in config.key\" typeahead-min-length=0 autocomplete=off></div><div><label>Value</label></div><div class=form-group><label class=sr-only for=value>Value</label> <input type=text id=value class=form-control ng-model=config.value placeholder=\"Première Donnée\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0></div><div class=form-group><label class=sr-only for=value2>Value2</label> <input type=text id=value2 class=form-control ng-model=config.value2 placeholder=\"Seconde Donnée\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0></div><div ng-repeat=\"line in config.lines\"><div class=\"form-group form-inline padding-bottom\"><label class=sr-only for=value2>Value {{$index + 2}}</label> <input type=text id=value2 class=form-control ng-model=line.value placeholder=\"Donnée {{$index + 3}}\" uib-typeahead=\"key for key in config.key\" typeahead-min-length=0> <button type=button class=\"btn btn-warning\" ng-click=graph.removeLine($index)><i class=\"fa fa-minus\"></i> Remove</button></div></div><button type=button class=\"btn btn-primary\" ng-click=graph.addLine()><i class=\"fa fa-plus\"></i> Ajouter une ligne</button><div><label>Type du graph</label></div><div class=form-group><label class=sr-only for=sample>Chart Type</label><select class=form-control ng-model=config.type><option value=line>Line</option><option value=bar>Bar</option><option value=horizontalBar>Horizontal Bar</option><option value=radar>Radar</option></select></div><div><label ng-click=\"isCollapsed = !isCollapsed\">Chart Option <span ng-hide=isCollapsed class=\"glyphicon glyphicon-triangle-bottom\" aria-hidden=true></span> <span ng-show=isCollapsed class=\"glyphicon glyphicon-triangle-top\" aria-hidden=true></span></label></div><div ng-show=isCollapsed><input type=checkbox ng-model=config.noPoint><label>No point</label><div ng-repeat=\"line in config.series\" ng-if=\"$index % 2 == 0\" class=row><div class=col-md-6><label for=color>{{line}} :</label><color-picker ng-model=config.color[$index] options=\"{required : false, format : \'hexString\'}\"></color-picker></div><div class=col-md-6 ng-if=\"config.series.length > $index +1\"><label for=color>{{config.series[$index +1]}} :</label><color-picker ng-model=config.color[$index+1] options=\"{required : false, format : \'hexString\'}\"></color-picker></div></div><div class=row><div class=col-md-6><label for=min>Valeur Minimum</label> <input type=text id=min ng-model=config.minValue></div><div class=col-md-6><label for=max>Valeur Maximum</label> <input type=text id=max ng-model=config.maxValue></div></div><label for=step>Step</label> <input type=text id=step ng-model=config.step></div></form>");
+$templateCache.put("{widgetsPath}/lineChart/src/view/view.html","<div><div ng-hide=graph.label class=\"alert alert-info\" role=alert>Please insert a url to the widget configuration</div><div ng-show=\"graph.label && !graph.load\"><div><canvas id=graph class=chart-base chart-type=graph.type chart-data=graph.value chart-labels=graph.label chart-series=graph.series chart-options=graph.options chart-colors=graph.color></canvas></div><div><p class=text-center>{{graph.desc}}</p></div><my-export chart=graph.chart></my-export></div><div ng-show=graph.load><div ng-include=\"\'/templates/directive/loader.html\'\"></div></div></div>");}]);
 
 
 
@@ -54,6 +54,7 @@ function lineChartController($rootScope, $scope, data, lineChartService){
     if (data.config.listener){
       $rootScope.$on('DatTest', function(events, args){
 
+        graph.config.expertReplace = args;
         graph.config.urlReplace = args[graph.config.slaveValue];
         graph.load = true;
         // Reload the widget
@@ -65,45 +66,41 @@ function lineChartController($rootScope, $scope, data, lineChartService){
         });
       });
     }
-  // Option for the chart --> See the chart.js options
-  this.options = {legend : {display : true, position :'bottom'}};
+    // Option for the chart --> See the chart.js options
+    this.options = {legend : {display : true, position :'bottom'}};
 
-  if (this.config.noPoint){
-    this.options.elements = {point : {hitRadius : 15, hoverRadius : 5, radius: 0}};
-  }
+    if (this.config.noPoint){
+      this.options.elements = {point : {hitRadius : 15, hoverRadius : 5, radius: 0}};
+    }
 
-  if (this.config.minValue || this.config.maxValue || this.config.step){
-    this.options.scales = {yAxes : [{
-      display:true,
-      type: 'linear',
-      ticks: {}
-    }]};
-    console.log(this.options);
-  }
+    if (this.config.minValue || this.config.maxValue || this.config.step){
+      this.options.scales = {yAxes : [{
+        display:true,
+        type: 'linear',
+        ticks: {}
+      }]};
+      console.log(this.options);
+    }
 
-  if (this.config.minValue){
-    this.options.scales.yAxes[0].ticks.min = parseInt(graph.config.minValue, 10);
-  }
+    if (this.config.minValue){
+      this.options.scales.yAxes[0].ticks.min = parseInt(graph.config.minValue, 10);
+    }
 
-  if (this.config.maxValue){
-    this.options.scales.yAxes[0].ticks.max = parseInt(graph.config.maxValue, 10);
-  }
+    if (this.config.maxValue){
+      this.options.scales.yAxes[0].ticks.max = parseInt(graph.config.maxValue, 10);
+    }
 
-  if (this.config.step){
-      this.options.scales.yAxes[0].ticks.stepSize = parseInt(graph.config.step, 10);
-  }
+    if (this.config.step){
+        this.options.scales.yAxes[0].ticks.stepSize = parseInt(graph.config.step, 10);
+    }
 
-  if (graph.config.color){
-    graph.color = [];
-    for( var k in graph.config.color){
-      graph.color[k] = graph.config.color[k];
+    if (graph.config.color){
+      graph.color = [];
+      for( var k in graph.config.color){
+        graph.color[k] = graph.config.color[k];
+      }
     }
   }
-
-
-
-
-}
 
 }
 lineChartController.$inject = ["$rootScope", "$scope", "data", "lineChartService"];
--- client/widgets/lineChart/src/view/lineChartController.js
+++ client/widgets/lineChart/src/view/lineChartController.js
@@ -23,6 +23,7 @@ function lineChartController($rootScope, $scope, data, lineChartService){
     if (data.config.listener){
       $rootScope.$on('DatTest', function(events, args){
 
+        graph.config.expertReplace = args;
         graph.config.urlReplace = args[graph.config.slaveValue];
         graph.load = true;
         // Reload the widget
@@ -34,44 +35,40 @@ function lineChartController($rootScope, $scope, data, lineChartService){
         });
       });
     }
-  // Option for the chart --> See the chart.js options
-  this.options = {legend : {display : true, position :'bottom'}};
+    // Option for the chart --> See the chart.js options
+    this.options = {legend : {display : true, position :'bottom'}};
 
-  if (this.config.noPoint){
-    this.options.elements = {point : {hitRadius : 15, hoverRadius : 5, radius: 0}};
-  }
+    if (this.config.noPoint){
+      this.options.elements = {point : {hitRadius : 15, hoverRadius : 5, radius: 0}};
+    }
 
-  if (this.config.minValue || this.config.maxValue || this.config.step){
-    this.options.scales = {yAxes : [{
-      display:true,
-      type: 'linear',
-      ticks: {}
-    }]};
-    console.log(this.options);
-  }
+    if (this.config.minValue || this.config.maxValue || this.config.step){
+      this.options.scales = {yAxes : [{
+        display:true,
+        type: 'linear',
+        ticks: {}
+      }]};
+      console.log(this.options);
+    }
 
-  if (this.config.minValue){
-    this.options.scales.yAxes[0].ticks.min = parseInt(graph.config.minValue, 10);
-  }
+    if (this.config.minValue){
+      this.options.scales.yAxes[0].ticks.min = parseInt(graph.config.minValue, 10);
+    }
 
-  if (this.config.maxValue){
-    this.options.scales.yAxes[0].ticks.max = parseInt(graph.config.maxValue, 10);
-  }
+    if (this.config.maxValue){
+      this.options.scales.yAxes[0].ticks.max = parseInt(graph.config.maxValue, 10);
+    }
 
-  if (this.config.step){
-      this.options.scales.yAxes[0].ticks.stepSize = parseInt(graph.config.step, 10);
-  }
+    if (this.config.step){
+        this.options.scales.yAxes[0].ticks.stepSize = parseInt(graph.config.step, 10);
+    }
 
-  if (graph.config.color){
-    graph.color = [];
-    for( var k in graph.config.color){
-      graph.color[k] = graph.config.color[k];
+    if (graph.config.color){
+      graph.color = [];
+      for( var k in graph.config.color){
+        graph.color[k] = graph.config.color[k];
+      }
     }
   }
-
-
-
-
-}
 
 }
